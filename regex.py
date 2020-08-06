5482a342cc7e7e41d0080932be0716a45eaa7cb9
ihorturchyn <61976424+ihorturchyn@users.noreply.github.com>
2020-08-06 11:57:09 +0300

    Create regex.py

---
+++ regex.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import re
+
+pattern = r'(\S{0,15})\s(\S{0,15}).*(\d{12})->(\d{12})\sevent type 2'
+file = open('example.txt').read()
+array = file.split(',')
+
+for i in (array):
+    mo = re.findall(pattern, i)
+    print(mo)
