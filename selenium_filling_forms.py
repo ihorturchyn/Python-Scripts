48899765f54352813710e594aadaf528fe7ac193
ihorturchyn <61976424+ihorturchyn@users.noreply.github.com>
2020-09-08 12:07:31 +0300

    Update selenium_filling_forms.py

--- selenium_filling_forms.py
+++ selenium_filling_forms.py
@@ -3,12 +3,12 @@ from selenium import webdriver
 import time
 driver = webdriver.Chrome()
 driver.get("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin")
-driver.find_element_by_id("identifierId").send_keys('ihorvturchyn@gmail.com')
+driver.find_element_by_id("identifierId").send_keys('email@gmail.com')
 time.sleep(3)
 driver.find_element_by_id("identifierNext").click()
 #time.sleep(5)
 
-# driver.find_element_by_name("password").send_keys('VerizonR523')
+# driver.find_element_by_name("password").send_keys('password')
 # driver.find_element_by_id("passwordNext").click()
 # time.sleep(5)
 
